0305d30c1f349762cc276c08de041578b77bba5c
Cleyton Rodrigues <88549697+cleytonsilvadev@users.noreply.github.com>
2022-02-25 20:25:27 -0300

    eclipse commmit
    atualização do eclipse

---
+++ src/githubTeste/hubteste.java
@@ -0,0 +1,15 @@
+package githubTeste;
+
+public class hubteste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double peso = 75;
+		double altura = 1.65;
+		double imc;
+		imc= peso/(altura * altura);
+		
+		System.out.printf("O seu IMC corresponde a %.3f" , imc);
+	}
+
+}
